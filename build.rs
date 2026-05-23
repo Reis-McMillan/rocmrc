685e50e40a22dc2b3cc1b1e8a0435fbaf5e81c23
Reis-McMillan <mcmillar@purdue.edu>
2026-05-23 14:29:39 -0400

    using bindgen for building hip ffi

--- build.rs
+++ build.rs
@@ -4,4 +4,19 @@ fn main() {
     println!("cargo:rustc-link-lib=dylib=amdhip64");
     println!("cargo:rustc-link-lib=dylib=hiprtc");
     println!("cargo:rerun-if-env-changed=ROCM_PATH");
+    println!("cargo:rerun-if-changed=src/driver/sys/wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("src/driver/sys/wrapper.h")
+        .clang_arg(format!("-I{rocm_path}/include"))
+        .allowlist_function("hip.*")
+        .allowlist_type("hip.*")
+        .allowlist_var("(hip|HIP)_.*")
+        .default_enum_style(bindgen::EnumVariation::NewType { is_bitfield: false, is_global: false })
+        .generate()
+        .expect("bindgen");
+
+    bindings
+        .write_to_file("src/driver/sys/sys.rs")
+        .expect("write bindings");
 }
